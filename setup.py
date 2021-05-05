2c6d70ff46eb6e04ef411d20b19ed60a6a2248fc
Nihan Pol <nihan.pol@nanograv.org>
2021-05-05 10:11:03 -0500

    re-worked install dependency order; clean up name

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='MAPS',
+    name='maps',
     url='https://github.com/NihanPol/MAPS',
     author='Nihan Pol',
     author_email='nihan.pol@nanograv.org',
     # Needed to actually package something
-    packages=['MAPS'],
+    packages=['maps'],
     # Needed for dependencies
-    install_requires=['numpy', 'scipy', 'sympy', 'astroML', 'enterprise-pulsar', 'PTMCMCSampler', 'healpy'],
+    install_requires=['numpy', 'scipy', 'sympy', 'astroML', 'PTMCMCSampler', 'healpy', 'enterprise-pulsar'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
