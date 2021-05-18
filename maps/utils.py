28e53980a71c9eaf666ba731ca26a12e54053b3e
Nihan Pol <nihan.pol@nanograv.org>
2021-05-18 11:01:40 -0500

    revert stupid mistake

--- maps/utils.py
+++ maps/utils.py
@@ -41,7 +41,7 @@ def angular_power_spectrum(pta_anis, clm, burn = 4000, clm_err = []):
     if pta_anis.mode == 'power_basis':
         new_clm2 = clm ** 2
     else:
-        new_clm2 = np.full((clm.shape[0], pta_anis.l_max), 0.0)
+        new_clm2 = np.full((clm.shape[0], (pta_anis.l_max + 1) ** 2), 0.0)
         for ii in range(clm.shape[0]):
             new_clm2[ii] = convert_blm_params_to_clm(pta_anis, clm[ii]) ** 2
             
