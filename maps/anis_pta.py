6c5a877bbf8348b274d48836c3e09bc92dd33fcb
Nihan Pol <33101514+NihanPol@users.noreply.github.com>
2021-06-25 09:18:15 -0500

    remove alpha=0 for S/N > 1 condition. doesn't make sense.

--- maps/anis_pta.py
+++ maps/anis_pta.py
@@ -226,8 +226,7 @@ class anis_pta():
         sn00 = clm00[0] / clm_err00[0]
         
         if sn00 >= 1.0:
-            print("There is enough S/N here to not require regularization! Returning zero.")
-            return 0
+            print(r"S/N for c$_00 > 1$")
         
         #Calculate the S/N for the c00 component over different alphas
         clm0 = np.full((len(alphas)), 0.0)
@@ -263,8 +262,7 @@ class anis_pta():
         sn00 = clm00[0] / clm_err00[0]
         
         if sn00 >= 1.0:
-            print("There is enough S/N here to not require regularization! Returning zero.")
-            return 0
+            print(r"S/N for c$_00 > 1$")
         
         #Calculate the S/N for the c00 component over different alphas
         clm0 = np.full((len(cutoffs)), 0.0)
