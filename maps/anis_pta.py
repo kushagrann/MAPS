dd3251e46f8b53c6694a5877c7bb88baa28fc26b
Nihan Pol <33101514+NihanPol@users.noreply.github.com>
2021-08-02 13:39:05 -0500

    Correctly interpret amplitude as log10_base in max_lkl

--- maps/anis_pta.py
+++ maps/anis_pta.py
@@ -392,7 +392,7 @@ class anis_pta():
 
         def residuals(x, obs_orf, obs_orf_err):
     
-            amp2 = x[0]
+            amp2 = 10 ** x[0]
             clm_pred = utils.convert_blm_params_to_clm(self, x[1:])
             sim_orf = amp2 * np.sum(clm_pred[:, np.newaxis] * self.Gamma_lm, axis = 0)    
             
